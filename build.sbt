1955b45dd298faf850a12da607b9afde26cd8e45
ilEnzio <westfury@gmail.com>
2022-08-09 21:11:00 -0400

    feat(CardTest): card generator

--- build.sbt
+++ build.sbt
@@ -3,7 +3,10 @@ name := "DMato"
 version := "0.1"
 
 scalaVersion := "2.13.8"
+
 val catsVersion = "2.7.0"
 
-libraryDependencies += "org.typelevel" %% "cats-core" % catsVersion
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.13" % "test"
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats-core" % catsVersion,
+  "org.scalatest" %% "scalatest" % "3.2.13" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.14.1" % "test")
--- src/test/scala/CardTest.scala
+++ src/test/scala/CardTest.scala
@@ -1,6 +1,24 @@
+import deck._
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalacheck.Prop
+import org.scalacheck.Gen
 
-// TODo figure out how to run tests
 
-class CardTest {
+
+class CardTest extends AnyFunSuite {
+
+  // Test the name of various Cards
+  //
+
+  val genSuit = Gen.oneOf(Suit.all)
+  val genRank = Gen.oneOf(Rank.all)
+  val genCard = for {
+    rank <- genRank
+    suit <- genSuit
+  } yield Card(rank, suit)
+
+  println(genCard.sample)
+  println(genCard.sample)
+  println(genCard.sample)
+
 }
