210e7952924f37f8cc03520e23bf2929289ac971
ilEnzio <westfury@gmail.com>
2022-08-09 22:25:33 -0400

    feat(cardTest): arbitrary for card generation

--- TASKLIST.md
+++ TASKLIST.md
@@ -4,7 +4,7 @@ Some Tasks:
 
 - Create Deck
 - ~~Generate random hole cards~~
-
+- Name/string repr of Card
 - Determine Hand Ranking(winning hand)
 - Calculate the Equity of each player
 - Handle up to 9 players
--- src/test/scala/CardTest.scala
+++ src/test/scala/CardTest.scala
@@ -2,13 +2,14 @@ import deck._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalacheck.Prop
 import org.scalacheck.Gen
-
-
+import org.scalacheck.Arbitrary
 
 class CardTest extends AnyFunSuite {
 
   // Test the name of various Cards
   //
+  // ToDO: move this generator; but where??
+  // card companion or data generation object?
 
   val genSuit = Gen.oneOf(Suit.all)
   val genRank = Gen.oneOf(Rank.all)
@@ -17,8 +18,15 @@ class CardTest extends AnyFunSuite {
     suit <- genSuit
   } yield Card(rank, suit)
 
+  implicit val arbCard = Arbitrary(genCard)
+
   println(genCard.sample)
   println(genCard.sample)
   println(genCard.sample)
+
+  // Properties of Deck
+  // Full deck has length of 52
+  // every card is unique
+  //
 
 }
