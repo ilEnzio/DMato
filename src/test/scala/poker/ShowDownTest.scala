9f6f00b15ad5fe3da0492c5b6704c1c6a28ce808
ilEnzio <westfury@gmail.com>
2022-08-18 16:23:36 -0400

    feat(ShowDown- HighCard Hand): evalation and test for high card hands

--- src/test/scala/poker/ShowDownTest.scala
+++ src/test/scala/poker/ShowDownTest.scala
@@ -4,7 +4,9 @@ import org.scalacheck.Prop.{forAll, propBoolean, AnyOperators}
 import org.scalacheck.Properties
 import cats.implicits.catsSyntaxPartialOrder
 import poker.OrderInstances._
-import project.DataGenerators.{genAceHigh, genHand, genHighCard, genStraightFlush}
+import project.DataGenerators.{genAceHigh, genHand, genHighCard, genStraightFlush, rankMap}
+
+import scala.util.Random.shuffle
 
 object ShowDownTest extends Properties("ShowDownTest") {
 
@@ -21,11 +23,47 @@ object ShowDownTest extends Properties("ShowDownTest") {
         "Ace vs Other" |: (ShowDown(List(aHigh, other)) ?= List(aHigh, other))
       }
   }
-// TODo this is failing because my AceGen (and HighCard gen!) are not varied enough.
-  property("HighCard - Two Ace High but second rank wins") = forAll(genAceHigh, genAceHigh) { (fst, snd) =>
-    if (fst.cards.sorted.reverse(1) > snd.cards.sorted.reverse(1))
-      (ShowDown(List(fst, snd)) ?= List(fst, snd))
-    else
-      (ShowDown(List(fst, snd)) ?= List(snd, fst))
+  // TODo this is failing because my AceGen (and HighCard gen!) are not varied enough.
+  property("HighCard - General test of all Corresponding cards") = forAll(genAceHigh) { original =>
+    val oCardList = original.cards.sorted.reverse
+
+    def createDupe(idx: Int): Hand = {
+      val cardIdxRank = oCardList(4).rank
+      val newCard =
+        if (cardIdxRank.value == 2) oCardList(idx)
+        else oCardList(idx).copy(rank = rankMap(cardIdxRank.value - 1))
+      original.copy(cards = oCardList.take(4) ++ List(newCard))
+    }
+
+    val winningHand = Hand(oCardList.take(5))
+    val dupe1       = createDupe(4)
+    val dupe2       = createDupe(3)
+    val dupe3       = createDupe(2)
+    val dupe4       = createDupe(1)
+
+    (ShowDown(List(winningHand, dupe1)) ?= List(winningHand, dupe1)) &&
+    (ShowDown(List(dupe2, winningHand)) ?= List(winningHand, dupe2)) &&
+    (ShowDown(List(dupe3, winningHand)) ?= List(winningHand, dupe3)) &&
+    (ShowDown(List(dupe4, winningHand)) =? List(winningHand, dupe4)) && ("Not Equal" |: (ShowDown(
+      List(dupe1, winningHand)
+    ) != List(dupe1, winningHand)))
+  }
+
+  property("HighCard - Multiple AHigh, and other HighCArd") = forAll(genAceHigh, genHighCard) { (aHigh, other) =>
+    (other.cards.sorted.reverse(0).rank != Ace) ==> {
+
+      val oCardList = aHigh.cards.sorted.reverse
+      def createDupe(idx: Int): Hand = {
+        val cardIdxRank = oCardList(4).rank
+        val newCard =
+          if (cardIdxRank.value == 2) oCardList(idx)
+          else oCardList(idx).copy(rank = rankMap(cardIdxRank.value - 1))
+        aHigh.copy(cards = oCardList.take(4) ++ List(newCard))
+      }
+      val winningHand = Hand(oCardList.take(5))
+      val dupeAHigh   = createDupe(4)
+      val testList    = shuffle(List(winningHand, other, dupeAHigh))
+      (ShowDown(testList)) ?= List(winningHand, dupeAHigh, other)
+    }
   }
 }
