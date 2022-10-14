058b89bbde9eabda69b4e10b07bbf60af9851a36
ilEnzio <westfury@gmail.com>
2022-10-14 01:34:55 -0400

    feat(StreetTest): PlayerStanding PreFlop; test no more than 4 players can have the winning HighCard.

--- src/test/scala/EquityTest/StreetTest.scala
+++ src/test/scala/EquityTest/StreetTest.scala
@@ -51,7 +51,19 @@ object StreetTest extends Properties("BoardState Tests") {
       val winningRank = winnerList.get.map(_._3).fold(HighCard(Two, List.empty[Card])) { case (s: Hand, v: Hand) =>
         if (handOrder.compare(s, v) > 0) s else v
       }
-      if ((winningRank == Pair) && (winnerList.get.size > 2)) false
+      if ((winningRank.score == 2) && (winnerList.get.size > 2)) false
+      else if (winningRank.score > 2) false
+      else true
+  }
+
+  property("Preflop: If the winner is a HighCard, no more than 4 players can be winning ") = forAll(genPreflopBoard) {
+    preFlop =>
+      val winnerList = PlayerStanding.winnerList(preFlop)
+
+      val winningRank = winnerList.get.map(_._3).fold(HighCard(Two, List.empty[Card])) { case (s: Hand, v: Hand) =>
+        if (handOrder.compare(s, v) > 0) s else v
+      }
+      if ((winningRank.score == 1) && (winnerList.get.size > 4)) false
       else if (winningRank.score > 2) false
       else true
   }
